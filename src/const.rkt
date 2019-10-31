325d39309ec0c0a0acf74280661d6f20c2896bb8
erkin <erkinbatu@gmail.com>
2019-10-31 06:46:39 +0300

    v0.1.0

--- src/const.rkt
+++ src/const.rkt
@@ -2,6 +2,9 @@
 
 (provide (all-defined-out))
 
+(define project-name "Chelengk")
+(define project-version "v0.1.0")
+
 (define song-directory "SymbTr/txt")
 
 (define (invert-alist alist)
@@ -11,3 +14,14 @@
         (invert-alist-iter (cdr alist)
                            (cons (cons (cdar alist) (caar alist)) result))))
   (invert-alist-iter alist '()))
+
+(define version-message
+  (format #<<version
+~a ~a
+Copyright (C) 2019 Erkin Batu Altunbaş
+
+Each file of this project's source code is subject
+to the terms of the Mozilla Public Licence v2.0
+https://mozilla.org/MPL/2.0/
+version
+          project-name project-version))
--- src/main.rkt
+++ src/main.rkt
@@ -1,9 +1,29 @@
 #lang racket/base
 
-(require "notes.rkt"
+(require racket/cmdline
+         racket/function
+         racket/pretty)
+(require threading)
+
+(require "const.rkt"
+         "notes.rkt"
          "scales.rkt"
          "songs.rkt")
 
 
 (module+ main
-(define songs (read-all-songs)))
+  (command-line
+   #:program project-name
+   #:once-each
+   (("--version" "-v")
+    "Show version and licence information"
+    (displayln version-message)
+    (exit)))
+
+  ;; Example use
+  (~>> (read-all-songs)
+       caddr
+       song-notes
+       (map note-holdrian)
+       (filter (curry < 200))
+       (for-each (compose pretty-print (curry hash-ref inverse-perdeler)))))
